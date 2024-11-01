b64ce9882382f944e53d0546aef1dce20edacade
ViggoSeerden <491216@student.fontys.nl>
2024-11-01 16:21:45 +0100

    initial

---
+++ mail.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/smtp"
+	"os"
+)
+
+func sendMail(email string) {
+	// Gmail SMTP server configuration.
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587" // TLS port for Gmail
+
+	// Sender and recipient information.
+	sender := "osso.online.site@gmail.com"
+	password := os.Getenv("APP_PASSWORD") // App Password if using 2FA
+	recipient := email
+
+	// Email content.
+	subject := "Subject: Test Email from Osso Online\n"
+	body := "This is a test email sent from the Osso Online Email Microservice, written in Go"
+	message := []byte(subject + "\n" + body)
+
+	// Set up authentication information.
+	auth := smtp.PlainAuth("", sender, password, smtpHost)
+
+	// Send email.
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, sender, []string{recipient}, message)
+	if err != nil {
+		fmt.Println("Error sending email:", err)
+		return
+	}
+	fmt.Println("Email sent successfully!")
+}
---
+++ receive.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"log"
+	"strconv"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+// func fib(n int) int {
+// 	if n == 0 {
+// 		return 0
+// 	} else if n == 1 {
+// 		return 1
+// 	} else {
+// 		return fib(n-1) + fib(n-2)
+// 	}
+// }
+
+func main() {
+	conn, err := amqp.Dial("amqp://localhost")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"send-email", // name
+		false,        // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	failOnError(err, "Failed to set QoS")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		for d := range msgs {
+			n, err := strconv.Atoi(string(d.Body))
+			failOnError(err, "Failed to convert body to integer")
+
+			log.Printf(" [.] fib(%d)", n)
+			sendMail("dev.viggo@gmail.com")
+			response := "Successfully sent email!"
+
+			err = ch.PublishWithContext(ctx,
+				"",        // exchange
+				d.ReplyTo, // routing key
+				false,     // mandatory
+				false,     // immediate
+				amqp.Publishing{
+					ContentType:   "text/plain",
+					CorrelationId: d.CorrelationId,
+					Body:          []byte(response),
+				})
+			failOnError(err, "Failed to respond.")
+
+			d.Ack(false)
+		}
+	}()
+
+	log.Printf("Now listening...")
+	<-forever
+}
